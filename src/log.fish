a7f3f210eeff2e58f2f7aa59d42eaf8a6f4b7912
fstemarie <14352349+fstemarie@users.noreply.github.com>
2026-04-23 19:58:06 -0400

    Fixed exit code bugs

--- src/log.fish
+++ src/log.fish
@@ -4,35 +4,39 @@ end
 
 function info --description 'Log the info to a file and to the screen' -a message logger
     # Create the log file if it doesn't exist
-    test ! -e $log && touch $log_file
+    test ! -e "$log" && touch $log_file
     set_color normal
     echo "[INFO] 🔹 $message" | tee -a $log
-    test -n "$logger" && logger -t (status basename) "[INFO] $message"
+    test -n "$logger"; and logger -t (status basename) "[INFO] $message"
+    return 0
 end
 
 function warning --description 'Log the warning to a file and to the screen' -a message logger
     # Create the log file if it doesn't exist
-    test ! -e $log && touch $log_file
+    test ! -e "$log" && touch $log_file
     set_color bryellow
     echo "[WARNING] ⚠️ $message" | tee -a $log
     set_color normal
-    test -n "$logger" && logger -t (status basename) "[INFO] $message"
+    test -n "$logger"; and logger -t (status basename) "[INFO] $message"
+    return 0
 end
 
 function error --description 'Log the error to a file and the logger' -a message logger
     # Create the log file if it doesn't exist
-    test ! -e $log && touch $log_file
+    test ! -e "$log" && touch $log_file
     set_color brred
     echo "[ERROR] ❌ $message" | tee -a $log
     set_color normal
-    test -n "$logger" && logger -t (status basename) "[INFO] $message"
+    test -n "$logger"; and logger -t (status basename) "[INFO] $message"
+    return 0
 end
 
 function success --description 'Log the info to a file and to the screen' -a message logger
     # Create the log file if it doesn't exist
-    test ! -e $log && touch $log_file
+    test ! -e "$log" && touch $log_file
     set_color brgreen
     echo "[INFO] ✅ $message" | tee -a $log
     set_color normal
-    test -n "$logger" && logger -t (status basename) "[INFO] $message"
+    test -n "$logger"; and logger -t (status basename) "[INFO] $message"
+    return 0
 end

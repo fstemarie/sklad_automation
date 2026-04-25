86231389c3797421c5871c9bdd2db3929f36f57e
fstemarie <14352349+fstemarie@users.noreply.github.com>
2026-04-25 16:29:49 -0400

    Ajoute script qui sauvegarde mes ebooks via rsync vers filelu

---
+++ src/backup/rsync/eBooks.bkp.fish
@@ -0,0 +1,48 @@
+#! /usr/bin/fish
+
+set src "/d/ruby/eBooks" # Variable qui contient le chemin du dossier à sauvegarder
+set dst "francois@rsync.filelu.com:/" # Variable qui contient le chemin du dossier de destination où les sauvegardes seront stockées
+set log "/var/log/automation/eBooks.rsync.tar.log" # Variable qui contient le chemin du fichier de log où les messages d'information et d'erreur seront enregistrés
+set secret_file "/home/francois/.secrets/filelu" # Variable qui contient le chemin du fichier qui contient le mot de passe de filelu
+
+# inclut le fichier log.fish pour utiliser les fonctions d'écriture de log
+if test (status dirname) = "/data/automation"
+    source /data/automation/tools/log.fish
+else
+    source /home/francois/development/automation/src/tools/log.fish
+end
+
+# Ecrit l'entete du log pour cette execution du script
+echo "
+
+-------------------------------------
+[[ Execution de  "(status basename)" ]]
+"(date -Iseconds)" 
+-------------------------------------
+" | tee -a $log
+
+info "Sauvegarde du dossier $src vers $dst"
+
+# Vérifie que la source existe et vérifie que la destination existe
+# Si le dossier source n'existe pas, alors il n'y a rien à sauvegarder
+info "Vérification de l'existence du dossier source"
+if test -d "$src"
+    success "Le dossier source existe"
+else
+    error "Le dossier source n'existe pas"
+    exit 1
+end
+
+# Transfer rsync
+info "Transfer des fichiers par rsync"
+sshpass -f $secret_file rsync \
+    --archive --verbose --delete --progress \
+    --itemize-changes --human-readable --stats \
+    --no-compress -e 'ssh -p 2222' \
+    "$src" "$dst" 2>&1 | tee -a $log
+# Vérifie si la commande tar a réussi
+if test $pipestatus[1] -ne 0
+    error "La sauvegarde a échoué"
+    exit 1
+end
+success "La sauvegarde a réussi"
